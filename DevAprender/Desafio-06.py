5671f4e8652da5315bdb4ff58d944fed4ef42758
Willian Veríssimo <168719237+WillVerissimo@users.noreply.github.com>
2024-09-15 05:21:03 +0000

    Adicionando `Desafio-06`

---
+++ DevAprender/Desafio-06.py
@@ -0,0 +1,9 @@
+# Através da criação de strings dinâmicos e os métodos de um string que acabou de aprender, use como base as variáveis a seguir para criar as seguintes frases
+a = "é"
+b = "MELHOR"
+c = "QUE"
+d = "feito"
+e = "perfeito"
+
+# É melhor FEITO que PERFEITO
+print(f"{a.upper()} {b.lower()} {d.upper()} {c.lower()} {e.upper()}")
