067a7a0a87670b15b288e202f0ce79cb94433d94
Willian Veríssimo <168719237+WillVerissimo@users.noreply.github.com>
2024-09-15 05:07:14 +0000

    Adicionando `Desafio-05`

---
+++ DevAprender/Desafio-05.py
@@ -0,0 +1,17 @@
+# Crie as seguintes strings dinâmicas:
+nome = "Carol"
+hobby = "ouvir música"
+
+print(f"Olá, sou a {nome} e gosto de {hobby}.")
+
+# Monte as seguintes palavras, sendo as sílabas como base
+
+b = "ba"
+parte2 = "nica"
+a = "a"
+r = "ri"
+parte1 = "eletrô"
+t = "te"
+
+# bateria eletrônica
+print(f"{b}{t}{r}{a} {parte1}{parte2}")
