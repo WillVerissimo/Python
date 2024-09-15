5ab7c825e79c667d1e6eb0df1ed5d969dd21639f
Willian Veríssimo <168719237+WillVerissimo@users.noreply.github.com>
2024-09-15 05:45:36 +0000

    Adicionando `Desafio-07`

---
+++ DevAprender/Desafio-07.py
@@ -0,0 +1,7 @@
+# Encontre o índice de 'o' dentro da variável biblioteca
+biblioteca = "Biblioteca"
+print(biblioteca[biblioteca.index("o")])
+
+# Usando a frase 'Desenvolvimento De Aplicações', imprima apenas o trecho 'De Aplicações'
+frase = "Desenvolvimento De Aplicações"
+print(frase[frase.rindex("D"):])
