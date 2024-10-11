77155e9206753cc67c9fac282d3dcb76a4cfa972
Willian Veríssimo <168719237+WillVerissimo@users.noreply.github.com>
2024-10-11 17:35:33 +0000

    Adcionando `Desafio-14.py` onde se começa a utilizar loops

---
+++ DevAprender/Desafio-14.py
@@ -0,0 +1,9 @@
+''' Desafio 1  🥇
+Usando um loop, exiba na tela: Estamos em X
+onde x é um valor iniciando em 18 e finalizando em 110
+Desafio 2
+Você precisa de 10 passos para finalizar uma tarefa, exiba na tela, usando loop for a seguinte frase =  "Realizando passo X"
+'''
+
+for contagem in range(18,111):
+    print(f'Realizando passo {contagem}')
