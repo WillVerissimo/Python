50083d823e4f02625e7aff607c271927568250ca
WillVerissimo <dev.willv@gmail.com>
2025-02-28 17:40:43 -0300

    Atualizando projeto de número 3

--- DevAprender/Pyautogui projects/Project_03.py
+++ DevAprender/Pyautogui projects/Project_03.py
@@ -4,7 +4,7 @@ def arrastar_documentos():
     moveTo(960,508,duration=1)
     dragTo(991,221,duration=1)
 
-total_documentos = 10
+total_de_documentos = 10 # Insira aqui o número total de documentos a serem transferidos
 
-for documento in range(total_documentos):
+for documento in range(total_de_documentos):
     arrastar_documentos()
